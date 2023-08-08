29705b52fed9d29b5e45d3bc3b2969d1756bdc04
HemaVarun9 <varunh263@gmail.com>
2023-08-08 22:41:30 +0530

    Updated.

---
+++ name.py
@@ -0,0 +1,5 @@
+first_name = "Hema"
+last_name = "Varun"
+full_name = first_name + last_name
+print(full_name)
+
---
+++ numpy.py
@@ -0,0 +1,9 @@
+x=4
+y=3
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)
+print("Division:",x/2)
+print("Modular Division with 2:",x%2)
+print("Power of 2:",x**2)
+print("Addition with 5 :",x+5)
